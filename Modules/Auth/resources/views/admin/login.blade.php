f4ac2851b929ad6cb7716f2480dbe1cf4900547e
hieu313 <106041165+hieu313@users.noreply.github.com>
2023-12-14 23:02:50 +0700

    Auth Module: render user login -> sidebar

--- Modules/Auth/resources/views/admin/login.blade.php
+++ Modules/Auth/resources/views/admin/login.blade.php
@@ -14,30 +14,30 @@
                 <div class="input-group mb-3">
                     <input type="email" name="email" value="{{ old('email') }}"
                            class="form-control @error('email') is-invalid @enderror" placeholder="Email">
-                    @error('email')
-                    <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                    @enderror
                     <div class="input-group-append">
                         <div class="input-group-text">
                             <span class="fas fa-envelope"></span>
                         </div>
                     </div>
-                </div>
-                <div class="input-group mb-3">
-                    <input type="password" name="password"
-                           class="form-control @error('password') is-invalid @enderror" placeholder="Password">
-                    @error('password')
+                    @error('email')
                     <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
                     </span>
                     @enderror
+                </div>
+                <div class="input-group mb-3">
+                    <input type="password" name="password"
+                           class="form-control @error('password') is-invalid @enderror" placeholder="Password">
                     <div class="input-group-append">
                         <div class="input-group-text">
                             <span class="fas fa-lock"></span>
                         </div>
                     </div>
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                 </div>
                 <div class="row">
                     <!-- /.col -->
